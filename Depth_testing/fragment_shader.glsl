cbddd88422f5a79a424aaaf8ee40f220c5c3d1af
gusrlLee <gusrl3204@naver.com>
2022-07-08 17:07:07 +0900

    opengl study depth study

---
+++ Depth_testing/fragment_shader.glsl
@@ -0,0 +1,20 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec2 TexCoords;
+
+// uniform sampler2D texture1;
+float near = 0.1;
+float far = 100.0;
+
+float LinearizeDepth(float depth) {
+    float z = depth * 2.0 - 1.0;
+    return (2.0 * near * far) / (far + near - z * (far - near));
+}
+
+void main()
+{
+    // FragColor = texture(texture1, TexCoords);
+    float detph = LinearizeDepth(gl_FragCoord.z) / far;
+    FragColor = vec4(vec3(detph), 1.0);
+}
